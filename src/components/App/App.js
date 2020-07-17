069aa7d95dd78a983ddd636be5f859d4b92f7086
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-17 15:33:34 -0500

    starts project

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    galleryItems: {
+      id: '',
+      path: '',
+      description: '',
+      likes: '',
+    },
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Gallery of my life</h1>
+        </header>
+        <br />
+        <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg" />
+      </div>
+    );
+  }
+}
+
+export default App;
